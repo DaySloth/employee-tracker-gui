912d1dd13a06cb194c8079d71f09a28d7b0c58d5
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-15 22:11:57 -0600

    Create thead.js

---
+++ employeetracker/src/components/table/tableHead/thead.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './thead.css';
+
+function TableHead() {
+    return (
+        <thead>
+            <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>DOB</th>
+            </tr>
+        </thead>
+    )
+
+};
+
+export default TableHead;
