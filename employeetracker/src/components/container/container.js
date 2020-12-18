c8c9978e06717ecfaf70475d2d296456700a1f78
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-17 20:09:22 -0600

    Update container.js

--- employeetracker/src/components/container/container.js
+++ employeetracker/src/components/container/container.js
@@ -1,6 +1,5 @@
 import { React, Component } from 'react';
 import SearchBar from '../searchbar/searchBar.js';
-import SortBtn from '../sortbutton/sortBtn.js';
 import Table from '../table/table.js';
 import API from '../../utils/API.js';
 
